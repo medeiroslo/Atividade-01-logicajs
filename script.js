dff415657f32780ff54bf5ac9a991e4191a0ae26
medeiroslo <medeirosslorrane@gmail.com>
2022-01-31 10:41:24 -0300

    firts commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let nome = "Lorrane Medeiros De Sousa";
-let idade=16
-let ano=2022
+let idade =  16
+let ano = 2022
 console.log(ano-idade)
 console.log(idade+ano)
 console.log('Estou mexendo no console.log');
-let quartaFeira=true
+let quartaFeira = true
 console.log(quartaFeira)
 console.log(typeof quartaFeira)
